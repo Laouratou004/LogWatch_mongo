e392b892f56e47746dfb2053ccc9eefde246ea07
Hadiatou Sow <hadiatousow365@gmail.com>
2026-05-29 10:19:24 +0000

    security(api): ajout de helmet et morgan

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const helmet = require('helmet');
+const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,6 +8,8 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
